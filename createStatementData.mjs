7b3a155b1b34d6911f9e3c22720646713b1bc41c
Baochuan Lu <lubaochuan@gmail.com>
2021-03-23 07:28:26 -0500

    Inline Function

--- createStatementData.mjs
+++ createStatementData.mjs
@@ -10,7 +10,7 @@ export default function createStatementData(invoice, plays){
     const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
-    result.amount = amountFor(result);
+    result.amount = calculator.amount;
     result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
@@ -35,10 +35,6 @@ export default function createStatementData(invoice, plays){
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
   }
 }
 
