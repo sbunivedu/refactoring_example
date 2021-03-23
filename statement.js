38ef0a01f51fe96bca1d98a1a5247f950a6a4f24
Baochuan Lu <lubaochuan@gmail.com>
2021-03-22 22:42:05 -0500

    Replace Temp with Query

--- statement.js
+++ statement.js
@@ -8,7 +8,7 @@ function statement (invoice, plays) {
     minimumFractionDigits: 2 }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     let thisAmount = amountFor(perf, play);
 
     // add volume credits
@@ -23,28 +23,32 @@ function statement (invoice, plays) {
   result += `Amount owed is ${format(totalAmount/100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
-}
 
-function amountFor(aPerformance, play) {
-  let result = 0;
-  switch (play.type) {
-  case "tragedy":
-    result = 40000;
-    if (aPerformance.audience > 30) {
-      result += 1000 * (aPerformance.audience - 30);
-    }
-    break;
-  case "comedy":
-    result = 30000;
-    if (aPerformance.audience > 20) {
-      result += 10000 + 500 * (aPerformance.audience - 20);
-    }
-    result += 300 * aPerformance.audience;
-    break;
-  default:
-    throw new Error(`unknown type: ${play.type}`);
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
   }
-  return result;
+
+  function amountFor(aPerformance, play) {
+    let result = 0;
+    switch (play.type) {
+    case "tragedy":
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
+      }
+      break;
+    case "comedy":
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
+      }
+      result += 300 * aPerformance.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+    }
+    return result;
+  }
 }
 
 const fs = require('fs');
